c009d06a3381df17915236f3c9f22dbea5b30fe3
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-10-23 23:51:02 +0200

    fixed files

--- config/database.php
+++ config/database.php
@@ -125,7 +125,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
@@ -148,4 +148,16 @@ return [
 
     ],
 
+    'listLinks' => [
+        'characters',
+        'comics',
+        'movies',
+        'tv',
+        'games',
+        'collectibles',
+        'videos',
+        'fans',
+        'news',
+        'shop'
+    ],
 ];
