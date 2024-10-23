4444e57510187f73fc4fc99a8162ec3f3a17d1fa
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-10-23 16:36:19 +0200

    created db.php in confing

---
+++ config/db.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'listLinks' => [
+        'characters',
+        'comics',
+        'movies',
+        'tv',
+        'games',
+        'collectibles',
+        'videos',
+        'fans',
+        'news',
+        'shop'
+    ],
+];
