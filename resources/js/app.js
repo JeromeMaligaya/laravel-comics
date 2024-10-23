671ae7fd2e2147c65de8f24e6b2b36ade69d05b6
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-10-23 16:36:38 +0200

    fixed app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,8 @@ import './bootstrap';
 
 import "~resources/scss/app.scss";
 
+import * as bootstrap from "bootstrap";
+
 import.meta.glob([
     '../img/**'
 ]);
-
-import * as bootstrap from "bootstrap";
