20b31fa4b88d60293a679c95f6f38d67b2e3f2b6
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-10-23 17:28:27 +0200

    created footerListLinks on db.php

--- config/db.php
+++ config/db.php
@@ -13,4 +13,52 @@ return [
         'news',
         'shop'
     ],
+
+    'footerListLinks' => [
+        [
+            'title' => 'DC COMICS',
+            'items' => [
+                ['name' => 'Characters'],
+                ['name' => 'Comics'],
+                ['name' => 'Movies'],
+                ['name' => 'TV'],
+                ['name' => 'Games'],
+                ['name' => 'Videos'],
+                ['name' => 'News'],
+            ]
+        ],
+        [
+            'title' => 'SHOP',
+            'items' => [
+                ['name' => 'Shop DC'],
+                ['name' => 'Shop DC Collectibles'],
+            ]
+        ],
+        [
+            'title' => 'DC',
+            'items' => [
+                ['name' => 'Terms Of Use'],
+                ['name' => 'Privacy policy (New)'],
+                ['name' => 'Ad Choices'],
+                ['name' => 'Advertising'],
+                ['name' => 'Jobs'],
+                ['name' => 'Subscriptions'],
+                ['name' => 'Talent Workshop'],
+                ['name' => 'CPSC Certificates'],
+                ['name' => 'Ratings'],
+                ['name' => 'Shop Help'],
+                ['name' => 'Contact Us'],
+            ]
+        ],
+        [
+            'title' => 'SITES',
+            'items' => [
+                ['name' => 'DC'],
+                ['name' => 'MAD Magazine'],
+                ['name' => 'DC Kids'],
+                ['name' => 'DC Universe'],
+                ['name' => 'DC Power Visa'],
+            ]
+        ],
+    ],
 ];
