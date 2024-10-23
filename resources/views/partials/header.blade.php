ba3605a27f5c8a8a9fdee48562fc99acff2b2b4d
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-10-23 17:07:39 +0200

    added #nav-header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,21 @@
-<header>
+<header id="main-header">
+    <div class="container">
+        <div id="nav-header">
+            <!-- logo -->
+            <a href="#" class="img-box"> <img class="img-fluid" src="{{Vite::asset('resources/img/dc-logo.png')}}" alt="logo"></a>
 
+            <!-- list links -->
+            <nav>
+                <ul id="list-links">
+                    @foreach ($listLinks as $link)
+                    <li class="nav-item">
+                        <a href="#">{{ strtoupper($link) }}</a>
+                    </li>
+                    @endforeach
+                </ul>
+            </nav>
+        </div>
+    </div>
 </header>
+
+
