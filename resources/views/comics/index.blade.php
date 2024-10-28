2def3592a8404c98d876aa06f5524cc77037c137
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-10-29 00:25:08 +0100

    added route-comicShow in href of a in index.php.blade

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,25 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-<section id="comics-list">
-        <div class="container">
-            {{-- {{ dd($comicsList) }} --}}
-            <ul class="list-comics">
-                @foreach ($comicsList as $comic)
-                    <li class="card-comics">
-                        <a href="#">
-                            <div class="img-box">
-                                <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}" />
-                            </div>
-                        </a>
-                        <a href="#">
-                            <h3>{{ $comic['series'] }}</h3>
-                        </a>
-                    </li>
-                @endforeach
-            </ul>
-        </div>
-</section>
+<main>
+    <section id="comics-list">
+            <div class="container">
+                {{-- {{ dd($comicsList) }} --}}
+                <ul class="list-comics">
+                    @foreach ($comicsList as $comic)
+                        <li class="card-comics">
+                            <a href="{{ route("route-comicShow", $loop->index) }}">
+                                <div class="img-box">
+                                    <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}" />
+                                </div>
+                            </a>
+                            <a href="{{ route("route-comicShow", $loop->index) }}">
+                                <h3>{{ $comic['series'] }}</h3>
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+    </section>
+</main>
 @endsection
 
 @php
